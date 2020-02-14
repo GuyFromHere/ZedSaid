b63a4412d26b64e9f3307f7d17f14576c388f620
GuyFromHere <47902660+GuyFromHere@users.noreply.github.com>
2020-02-14 11:36:38 -0800

    More styling. Changed handlebar rendering to allow for html in quotes.

--- db/schema.sql
+++ db/schema.sql
@@ -28,12 +28,12 @@ create table quotes
 		(age, quote, context)
 	values
 		(2, "What if you went to the grocery store and never came back?", "Lying in bed with Dad."),
-		(4, "Me: We're terrible parents, huh? Zed: No Dad, just you.", "We let them have extra screentime or something."),
-		(4, "Me: Zinnia, tell me about your hopes and dreams? Zed: I wish you were dead. I mean I hope you die.", "Lying in bed, trying to get her to settle down."),
+		(4, "Me: We're terrible parents, huh? <br>Zed: No Dad, just you.", "We let them have extra screentime or something."),
+		(4, "Me: Zinnia, tell me about your hopes and dreams? <br>Zed: I wish you were dead. I mean I hope you die.", "Lying in bed, trying to get her to settle down."),
 		(4, "If I get married and my husband is anything like you I'm DEFINITELY getting a divorce.", "Lying in bed with Dad."),
 		(3, "Me: Hi Zinnia! Zed: Dad, STOP IT!", "Randomly happens about once a month."),
 		(4, "Maybe you're just being terrible again?", "I was mad at my computer."),
         (4, "Dad. You are such an idiot.", "Many times for various reasons."),
         (4, "What if you were married to someone who was just like Mom, but who wasn't my mom?", "Lying in bed again."),
-        (3, "Me: What can I say? I love farting. Zed: You can say excuse me!", "She accused me of farting."),
+        (3, "Me: What can I say? I love farting. <br>Zed: You can say excuse me!", "She accused me of farting."),
         (4, "I love all of my family 100. Except for you, Dad.", "Random zinger inserted in to a conversation.")
