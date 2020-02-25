c4aa5115e14d1fb4ed105d84ebf53597b0eef61c
Dan Dismuke <dandismuke@gmail.com>
2020-02-24 21:30:49 -0800

    Reconfigured as static page for GitHub Pages because heroku keeps crashing.

--- public/assets/scripts/javascript.js
+++ public/assets/scripts/javascript.js
@@ -1,33 +1,44 @@
+const quotes = [
+    "Me: We're terrible parents, huh? <br>Zed: No Dad, just you.",
+	"Me: Zinnia, tell me about your hopes and dreams? <br>Zed: I wish you were dead. I mean I hope you die.",
+	"If I get married and my husband is anything like you I'm DEFINITELY getting a divorce.",
+	"Me: Hi Zinnia! <br>Zed: Dad, STOP IT!",
+	"Maybe you're just being terrible again?",
+	"Dad. You are such an idiot.",
+	"What if you were married to someone who was just like Mom, but who wasn't my mom?",
+	"Me: What can I say? I love farting. <br>Zed: You can say excuse me!",
+	"I love all of my family 100. Except for you, Dad.",
+	"What if you went to the grocery store and never came back?"
+];
+
 $(function() {
-	$("#next").on("click", e => {
-		$.get("/", data => {
-			location.reload();
-		});
-	});
 
-	$("#addForm").on("submit", e => {
-		const age = $("#newAge").val();
-		const quote = $("#newQuote").val();
-		const context = $("#newContext").val();
-
-		const newQuote = {
-			age: $("#newAge").val(),
-			quote: $("#newQuote").val(),
-			context: $("#newContext").val()
-		};
-
-		$.ajax("/api/add_quote", {
-			type: "POST",
-			data: newQuote
-		}).then(() => {
-			location.reload();
-		});
-	});
-
-	getQuote = function() {
-		$.get("/", data => {
+	getQuote = function(quote) {
+		/* $.get("/", data => {
 			location.href = "/";
-		});
+		}); */	
 	};
-	setInterval(getQuote, 5500);
+
+	function intervalForEach (array, iteratee, delay) {
+		let current = 0
+		let interval = setInterval(() => {
+		if (current === array.length) {
+			current = 0;
+		} else {
+			iteratee(array[current])
+			current++
+		}
+		}, delay)
+	}
+	
+	// this will be applied to each item in the array
+	function updateQuote (quote) {
+		setTimeout(() => {
+			console.log(quote)
+			$('#quote').html(quote);
+		}, 100) 
+	}
+
+	intervalForEach(quotes, updateQuote, 6000);
+	
 });
